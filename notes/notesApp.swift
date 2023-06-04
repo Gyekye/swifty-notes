b8a49125451b08457a8bdd3f3e4080ae60409cb6
Richmond Gyekye <mrgyekye740@gmail.com>
2023-06-04 23:11:45 +0000

    Initial Commit

---
+++ notes/notesApp.swift
@@ -0,0 +1,17 @@
+//
+//  notesApp.swift
+//  notes
+//
+//  Created by richmond gyekye on 04/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct notesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
